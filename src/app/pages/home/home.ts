19f811deb943be1bb3f648de0644f92ad1501f85
Fernando <ferparra9908@gmail.com>
2026-04-29 23:13:09 -0500

    Commit Inicial

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Hero } from '../../components/hero/hero';
+import { About } from '../../components/about/about';
+import { Skills } from '../../components/skills/skills';
+import { Projects } from '../../components/projects/projects';
+import { Contact } from '../../components/contact/contact';
+
+@Component({
+  selector: 'app-home',
+  imports: [Hero, About, Skills, Projects, Contact],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {}
